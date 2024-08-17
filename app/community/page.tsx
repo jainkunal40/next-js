fe5686a8cff1297fd7538d2e425a21693d14a246
Kunal Jain <kunal.jain@thoughtworks.com>
2024-08-17 23:32:32 +0530

    Added more routes

---
+++ app/community/page.tsx
@@ -0,0 +1,3 @@
+export default function CommunityPage() {
+  return <h1>Community Home page</h1>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,22 @@
-import Header from "@/components/hearder";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <Header />
-      <p>🔥 Let&apos;s get started! 🔥</p>
-      <p>
-        {/* use link instead of a tag to create client side navigation */}
-        <Link href="/awesome">Go to the awesome page</Link>
-      </p>
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Time to get started!
+      </h1>
+      <div>
+        <p>
+          <Link href={"/meals"} />
+        </p>
+        <p>
+          <Link href={"/meals/share"} />
+        </p>
+        <p>
+          <Link href={"/community"} />
+        </p>
+      </div>
     </main>
   );
 }
